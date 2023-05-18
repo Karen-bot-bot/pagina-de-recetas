3594db6df1642d2f9376c1212024d90eb97f9843
Karen Luna <karenlunafeldheim@gmail.com>
2023-05-17 23:47:04 -0300

    Diseño responsive

--- js/carrusel.js
+++ js/carrusel.js
@@ -4,7 +4,7 @@ var cardWidth = $(".carousel-item").width();
 var scrollPosition = 0;
 
 $(".carousel-control-next").on("click", function () {
-  if (scrollPosition < carouselWidth - cardWidth * 4) {
+  if (scrollPosition < carouselWidth - cardWidth * 3) {
     //check if you can go any further
     scrollPosition += cardWidth; //update scroll position
     $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600); //scroll left
