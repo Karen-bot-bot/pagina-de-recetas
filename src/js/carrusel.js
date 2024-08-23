713398c10e15d825995d2708429aac7c20c3afed
Karen Luna <karenlunafeldheim@gmail.com>
2024-08-23 16:33:10 -0300

    Añadir nuevo inicio

--- src/js/carrusel.js
+++ src/js/carrusel.js
@@ -1,6 +1,6 @@
+/*
 var carouselWidth = $(".carousel-inner")[0].scrollWidth;
 var cardWidth = $(".carousel-item").width();
-
 var scrollPosition = 0;
 
 $(".carousel-control-next").on("click", function () {
@@ -14,5 +14,37 @@ $(".carousel-control-prev").on("click", function () {
   if (scrollPosition > 0) {
     scrollPosition -= cardWidth;
     $(".carousel-inner").animate({ scrollLeft: scrollPosition }, 600);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var element = document.querySelector(".carousel-inner");
+  if (element.classList.contains("overflow-hidden")) {
+    element.classList.remove("overflow-hidden");
+  }
+}); */
+
+var carouselInner = $(".carousel-inner");
+var cardWidth = $(".carousel-item").outerWidth(true);
+var scrollPosition = 0;
+
+$(".carousel-control-next").on("click", function () {
+  if (scrollPosition < carouselInner[0].scrollWidth - carouselInner.width()) {
+    scrollPosition += cardWidth; // Actualiza la posición de desplazamiento
+    carouselInner.animate({ scrollLeft: scrollPosition }, 600);
+  }
+});
+
+$(".carousel-control-prev").on("click", function () {
+  if (scrollPosition > 0) {
+    scrollPosition -= cardWidth;
+    carouselInner.animate({ scrollLeft: scrollPosition }, 600);
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var element = document.querySelector(".carousel-inner");
+  if (element.classList.contains("overflow-hidden")) {
+    element.classList.remove("overflow-hidden");
   }
 });
